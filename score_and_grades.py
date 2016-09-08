9783bcb4db1e284a7e5129498d233314fc0b9957
dhurataK <dhuratakamberi_@hotmail.com>
2016-09-08 18:27:51 +0200

    Updated scores and grades assignments

--- score_and_grades.py
+++ score_and_grades.py
@@ -2,7 +2,9 @@ def getGrade():
     print "Scores and Grades"
     for i in range(0,9):
         ip = input()
-        if (ip >= 60 and ip <= 69):
+        if(ip < 60):
+            print "You failed the exam. Your score is: "+str(ip)+" Good luck next time!"
+        elif (ip >= 60 and ip <= 69):
             print "Score: "+str(ip)+"; Your grade is D"
         elif (ip >= 70 and ip <= 79):
             print "Score: "+str(ip)+"; Your grade is C"
@@ -10,6 +12,8 @@ def getGrade():
             print "Score: "+str(ip)+"; Your grade is B"
         elif (ip >= 90 and ip <= 100):
             print "Score: "+str(ip)+"; Your grade is A"
+        elif (ip > 100):
+            print "Invalid score! "+str(ip)
     print "End of the program. Bye! "
 
 getGrade()

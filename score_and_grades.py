3d902c79e5e9851815f5a2a6248c4f30ccce6642
dhurataK <dhuratakamberi_@hotmail.com>
2016-09-08 13:01:43 +0200

    Added Scores and Grades assignment in Python

---
+++ score_and_grades.py
@@ -0,0 +1,15 @@
+def getGrade():
+    print "Scores and Grades"
+    for i in range(0,9):
+        ip = input()
+        if (ip >= 60 and ip <= 69):
+            print "Score: "+str(ip)+"; Your grade is D"
+        elif (ip >= 70 and ip <= 79):
+            print "Score: "+str(ip)+"; Your grade is C"
+        elif (ip >= 80 and ip <= 89):
+            print "Score: "+str(ip)+"; Your grade is B"
+        elif (ip >= 90 and ip <= 100):
+            print "Score: "+str(ip)+"; Your grade is A"
+    print "End of the program. Bye! "
+
+getGrade()

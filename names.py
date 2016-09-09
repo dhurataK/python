840756ad9059d5ab98a1d2f5b43dc72a4edc62fa
dhurataK <dhuratakamberi_@hotmail.com>
2016-09-09 19:29:13 +0200

    Added names.py assignment!

---
+++ names.py
@@ -0,0 +1,32 @@
+#PART 1
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+# for idx in students:
+#     print idx['first_name'] + " "+idx['last_name']
+#
+
+#PART 2
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+for key, data in users.items():
+    print key
+    number = 0
+    for value in data:
+        number += 1
+        name = value['first_name']+value['last_name']
+        nameLength = len(name)
+        print  str(number)+" - "+name.upper() +" - "+str(nameLength)
